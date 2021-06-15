01d80f02497e051e6c7b48b7286361131bbb4c3b
Indra Saputra <flame.fire99@gmail.com>
2021-06-15 16:00:52 +0700

    test: unit test for error entity

--- entity/error_test.go
+++ entity/error_test.go
@@ -1,1 +1,25 @@
 package entity_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/indrasaputra/orvosi/entity"
+)
+
+func TestErrInternal(t *testing.T) {
+	t.Run("success get internal error", func(t *testing.T) {
+		err := entity.ErrInternal("")
+
+		assert.Contains(t, err.Error(), "rpc error: code = Internal")
+	})
+}
+
+func TestErrEmptyMedicalRecord(t *testing.T) {
+	t.Run("success get empty medical record error", func(t *testing.T) {
+		err := entity.ErrEmptyMedicalRecord()
+
+		assert.Contains(t, err.Error(), "rpc error: code = InvalidArgument")
+	})
+}
